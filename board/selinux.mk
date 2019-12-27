a8a13d7e33c36482fa92123d4d65997136641486
Adrian DC <radian.dc@gmail.com>
2019-12-27 11:42:54 +0100

    sepolicy: Work-in-progress for AOSP Q
    Change-Id: I3cdd82ce35e0e21c80f3059498fdf9c4e153af78

--- board/selinux.mk
+++ board/selinux.mk
@@ -2,6 +2,9 @@
 BOARD_SEPOLICY_DIRS += \
     $(DEVICE_PATH)/sepolicy
 
+# LineageOS legacy sepolicies
+include device/qcom/sepolicy-legacy/sepolicy.mk
+
 # LineageOS device sepolicies
 ifeq ($(BOARD_AOSP_BASED),)
 BOARD_SEPOLICY_DIRS += \
